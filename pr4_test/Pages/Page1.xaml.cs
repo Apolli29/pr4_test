2bb4fdfd6d06302a1eafe5632cca2fa983ec9b77
234922 <234922@KB37-120-C15>
2026-02-24 14:05:19 +0300

    страница 1

--- pr4_test/Pages/Page1.xaml.cs
+++ pr4_test/Pages/Page1.xaml.cs
@@ -24,5 +24,75 @@ namespace pr4_test.Pages
         {
             InitializeComponent();
         }
+        private void BtnCalc_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            if (!double.TryParse(txtX.Text, out double x) ||
+
+            !double.TryParse(txtY.Text, out double y) ||
+
+            !double.TryParse(txtZ.Text, out double z))
+
+            {
+
+                MessageBox.Show("Введите корректные данные!");
+
+                return;
+
+            }
+
+
+            if (Math.Abs(x) > 1)
+
+            {
+
+                MessageBox.Show("|x| должно быть ≤ 1 для arccos(x)");
+
+                return;
+
+            }
+
+
+            double denominator = Math.Abs(x - y) * z + x * x;
+
+
+            if (denominator == 0)
+
+            {
+
+                MessageBox.Show("Знаменатель не может быть равен 0");
+
+                return;
+
+            }
+
+
+            double gamma =
+
+            5 * Math.Atan(x) -
+
+            0.25 * Math.Acos(x) *
+
+            ((x + 3 * Math.Abs(x - y) + x * x) / denominator);
+
+
+            txtResult.Text = gamma.ToString("F4");
+
+        }
+
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            txtX.Clear();
+
+            txtY.Clear();
+
+            txtZ.Clear();
+
+            txtResult.Clear();
+        }
     }
 }
