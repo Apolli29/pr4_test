2f466845658f669060270b8679bb74851d25e057
234922 <234922@KB37-120-C15>
2026-02-24 13:57:44 +0300

    добавлены страницы

--- pr4_test/MainWindow.xaml.cs
+++ pr4_test/MainWindow.xaml.cs
@@ -12,17 +12,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using pr4_test.Pages;
 
 namespace pr4_test
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+   
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new Page1());
         }
+        private void BtnPage1_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            MainFrame.Navigate(new Page1());
+
+        }
+
+
+        private void BtnPage2_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            MainFrame.Navigate(new Page2());
+
+        }
+
+
+        private void BtnPage3_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            MainFrame.Navigate(new Page3());
+
+        }
+
+
+        protected override void OnClosing(CancelEventArgs e)
+
+        {
+
+            if (MessageBox.Show("Вы уверены, что хотите выйти?",
+
+            "Выход",
+
+            MessageBoxButton.YesNo) == MessageBoxResult.No)
+
+            {
+
+                e.Cancel = true;
+
+            }
+
+        }
+
     }
+
 }
